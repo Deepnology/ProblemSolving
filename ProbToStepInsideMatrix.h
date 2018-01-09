ac4e864483767a7e1a594741480951ac86ca9152
Dave Feng <dave@deepnology.com>
2018-01-09 12:11:30 -0500

    Knight Probability in Chessboard

--- ProbToStepInsideMatrix.h
+++ ProbToStepInsideMatrix.h
@@ -14,6 +14,8 @@ Given a NxN matrix representing an island.
 At a given location x, y in the matrix, you can step up, down, left, and right for a particular number of steps.
 Calculate the probability at location x, y with a number of steps that you still remain inside the matrix.
 O(steps * N^2) time, O(steps * N^2) space
+
+Leetcode: Knight Probability in Chessboard
 */
 class ProbToStepInsideMatrix
 {
@@ -34,7 +36,6 @@ public:
 		std::cout << "ProbToStepInsideMatrix for \"" << N << "x" << N << "\", \"" << steps << "\" steps at [" << x << "," << y << "]: " << res << std::endl;
 		return res;
 	}
-
 	double dfsRecur(int N, int curX, int curY, int curSteps, std::vector<std::vector<std::vector<double> > > & dp)
 	{
 		if (curSteps == 0)//reaches end and still remain inside matrix: prob is 1
@@ -53,6 +54,43 @@ public:
 		if (curY < (N - 1))
 			prob += 0.25 * this->dfsRecur(N, curX, curY + 1, curSteps - 1, dp);
 		return dp[curX][curY][curSteps - 1] = prob;
+	}
+
+	//Leetcode: Knight Probability in Chessboard
+	double KnightProbability(int N, int K, int r, int c)
+	{
+		if (K == 0)
+			return 1.0;
+		std::vector<std::vector<std::vector<double>>> dp(N, std::vector<std::vector<double>>(N, std::vector<double>(K, -1.0)));
+		recur(N, r, c, K, dp);
+		return dp[r][c][K-1];
+	}
+	double recur(int N, int row, int col, int steps, std::vector<std::vector<std::vector<double>>> & dp)
+	{
+		if (steps == 0)
+			return 1.0;
+		if (dp[row][col][steps-1] != -1.0)
+			return dp[row][col][steps-1];
+		double prob = 0;
+		if (row+2 < N && col-1 >= 0)
+			prob += 1.0/8.0 * recur(N, row+2, col-1, steps-1, dp);
+		if (row+2 < N && col+1 < N)
+			prob += 1.0/8.0 * recur(N, row+2, col+1, steps-1, dp);
+		if (row-2 >= 0 && col-1 >= 0)
+			prob += 1.0/8.0 * recur(N, row-2, col-1, steps-1, dp);
+		if (row-2 >= 0 && col+1 < N)
+			prob += 1.0/8.0 * recur(N, row-2, col+1, steps-1, dp);
+
+		if (col+2 < N && row-1 >= 0)
+			prob += 1.0/8.0 * recur(N, row-1, col+2, steps-1, dp);
+		if (col+2 < N && row+1 < N)
+			prob += 1.0/8.0 * recur(N, row+1, col+2, steps-1, dp);
+		if (col-2 >= 0 && row-1 >= 0)
+			prob += 1.0/8.0 * recur(N, row-1, col-2, steps-1, dp);
+		if (col-2 >= 0 && row+1 < N)
+			prob += 1.0/8.0 * recur(N, row+1, col-2, steps-1, dp);
+
+		return dp[row][col][steps-1] = prob;
 	}
 };
 /*
