18e5b89d042f9a8b0df391b38cc9d9fd8fa5b852
Dave Feng <dave@deepnology.com>
2018-02-12 10:20:12 -0500

    add MatchCombinationSumsFrom2Arrays

---
+++ MatchCombinationSumsFrom2Arrays.h
@@ -0,0 +1,13 @@
+#ifndef _MATCH_COMBINATION_SUMS_FROM_2_ARRAYS_H
+#define _MATCH_COMBINATION_SUMS_FROM_2_ARRAYS_H
+#include "Debug.h"
+/*
+given two integer arrays with unique integers in each array, you can cancel out matched combination sums from each array as many as you can, return remaining numbers in each array
+ex: {1, 7, 5, 8} and {1, 10, 5}, return {5} and { }
+ */
+class MatchCombinationSumsFrom2Arrays
+{
+public:
+
+};
+#endif
