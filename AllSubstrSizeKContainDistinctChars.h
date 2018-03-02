25448f18247519ce9c4e97017795debb8301749d
Dave Feng <dave@deepnology.com>
2018-03-02 17:21:50 -0500

    add comment

--- AllSubstrSizeKContainDistinctChars.h
+++ AllSubstrSizeKContainDistinctChars.h
@@ -1,8 +1,27 @@
+/*AmazonOA*/
 #ifndef _ALL_SUBSTR_SIZE_K_CONTAIN_DISTINCT_CHARS_H
 #define _ALL_SUBSTR_SIZE_K_CONTAIN_DISTINCT_CHARS_H
 #include "Debug.h"
 /*
-Amazon
+Michelle has created a word game for her students. The word game begins with Michelle writing a string and a number, K, on the board.
+The students have to find the substrings of size K with K distinct characters.
+Write an algorithm to help the students find the correct answer.
+If the given string does not have K distinct characters then output an empty list.
+Input
+The input to the function/method consists of two arguments - inputStr, representing the number, K, written by the teacher on the board.
+Output
+Return distinct substrings of input string of size K with K distinct characters
+Constraints
+0 <= num <= 26
+Examples
+Input
+inputStr = awaglknagawunagwkwagl
+num = 4
+Output:
+{wagl, aglk, glkn, lkna, knag, gawu, awun, wuna, unag, nagw, agwk, kwag}
+Note that "wagl" appeared twice, but should output only one
+
+
 Given a string s (only lowercase chars) and a integer K, find all substrings in s with size K that contain distinct chars.
 see also LongestSubstrWoRepeatChars.h, LongestSubArrWDistinctEntries.h
  */
