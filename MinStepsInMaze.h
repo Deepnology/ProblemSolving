acbd42073c8d9d491730a0a6140d2b78d91a0ec5
Dave Feng <dave@deepnology.com>
2018-03-12 18:52:34 -0400

    add

--- MinStepsInMaze.h
+++ MinStepsInMaze.h
@@ -1,4 +1,4 @@
-/*AmazonOA*/
+/*Facebook*AmazonOA*/
 #ifndef _MIN_STEPS_IN_MAZE_H
 #define _MIN_STEPS_IN_MAZE_H
 #include "Debug.h"
@@ -68,12 +68,13 @@ public:
         std::vector<std::vector<int>> dir({{-1,0},{1,0},{0,1},{0,-1}});
         std::queue<int> que;
         int dist = 0;
-        que.push(0);
+        que.push(start[0]*M+start[1]);
+        visit[start[0]][start[1]] = 1;
         bool reach = false;
         while (!que.empty())
         {
             int levelCount = que.size();
-            for (int count = 0; count < levelCount; ++count)
+            while (levelCount--)
             {
                 auto p = que.front();
                 que.pop();
@@ -98,6 +99,7 @@ public:
             if (reach) break;
             ++dist;
         }
+        if (!reach) dist = -1;
 
         Debug::Print2D<int>()(visit, false);
         Debug::Print2D<int>()(maze, false);
@@ -140,5 +142,100 @@ Row#4	= 0, 0, 1, 0, 1, 0
 Row#5	= 0, 0, 0, 0, 1, 0
 
 MinStepsInMaze BFS_Queue for above maze from [0, 0] to [5, 5]: 16
+ */
+
+class MinStepsInMazeLeftToRight //from leftmost column to rightmost column
+{
+public:
+    MinStepsInMazeLeftToRight(){}
+
+    int BFS_Queue(const std::vector<std::vector<int>> & maze)
+    {
+        int N = maze.size();
+        if (N == 0) return 0;
+        int M = maze[0].size();
+        std::vector<std::vector<std::vector<int>>> parent(N, std::vector<std::vector<int>>(M, std::vector<int>(2, -1)));
+        std::vector<std::vector<int>> visit(N, std::vector<int>(M, 0));
+        std::vector<std::vector<int>> dir({{-1,0},{1,0},{0,1},{0,-1}});
+        std::queue<int> que;
+        int dist = 0;
+
+        for (int i = 0; i < N; ++i)
+            if (maze[i][0] == 0)
+                que.push(i*M+0); //add all 0s in leftmost column to queue
+
+        bool reach = false;
+        std::vector<int> end;
+        while (!que.empty())
+        {
+            int levelCount = que.size();
+            for (int count = 0; count < levelCount; ++count)
+            {
+                auto p = que.front();
+                que.pop();
+                int i = p / M;
+                int j = p % M;
+                if (j == M-1)
+                {
+                    reach = true;
+                    end = {i, j};
+                    break;
+                }
+                for (int k = 0; k < 4; ++k)
+                {
+                    int ii = i + dir[k][0];
+                    int jj = j + dir[k][1];
+                    if (ii >= 0 && ii < N && jj >= 0 && jj < M && maze[ii][jj] == 0 && !visit[ii][jj])
+                    {
+                        visit[ii][jj] = 1;
+                        que.push(ii * M + jj);
+                        parent[ii][jj] = {i, j};
+                    }
+                }
+            }
+            if (reach) break;
+            ++dist;
+        }
+        if (!reach) dist = -1;
+
+        Debug::Print2D<int>()(visit, false);
+        Debug::Print2D<int>()(maze, false);
+        std::cout << "MinStepsInMaze BFS_Queue for above maze from left to right: " << dist << std::endl;
+
+        if (reach)
+        {
+            std::vector<std::vector<int>> path;
+            int d = dist;
+            while (d--)
+            {
+                path.push_back(end);
+                end = parent[end[0]][end[1]];
+            }
+            std::reverse(path.begin(), path.end());
+            std::cout << "Path: " << Debug::ToStr1D<int>()(path) << std::endl;
+        }
+
+        return dist;
+    }
+};
+/*
+[rY][cX]
+Row#0	= 0, 0, 0, 1, 0, 0
+Row#1	= 0, 0, 1, 1, 1, 1
+Row#2	= 0, 1, 1, 0, 0, 0
+Row#3	= 1, 0, 1, 1, 1, 0
+Row#4	= 1, 1, 0, 1, 0, 0
+Row#5	= 1, 1, 1, 1, 1, 0
+
+[rY][cX]
+Row#0	= 0, 1, 1, 0, 1, 1
+Row#1	= 1, 1, 0, 0, 0, 0
+Row#2	= 1, 0, 0, 1, 1, 0
+Row#3	= 0, 1, 0, 0, 0, 1
+Row#4	= 0, 0, 1, 0, 1, 0
+Row#5	= 0, 0, 0, 0, 0, 1
+
+MinStepsInMaze BFS_Queue for above maze from left to right: 11
+Path: [5,1], [5,2], [5,3], [4,3], [3,3], [3,2], [2,2], [1,2], [1,3], [1,4], [1,5]
  */
 #endif
