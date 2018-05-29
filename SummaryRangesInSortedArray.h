3621a20a694eb9f0a8dd06b0cbc89116f9f089eb
Dave Feng <dave@deepnology.com>
2018-05-29 17:57:36 -0400

    Better

--- SummaryRangesInSortedArray.h
+++ SummaryRangesInSortedArray.h
@@ -54,8 +54,31 @@ public:
 		std::cout << "SummaryRangesInSortedArray for \"" << Debug::ToStr1D<int>()(nums) << "\": " << Debug::ToStr1D<std::string>()(res) << std::endl;
 		return res;
 	}
+
+	std::vector<std::string> Better(const std::vector<int> & nums)
+	{
+		int N = nums.size();
+		std::vector<std::string> res;
+		int begin = 0;
+		while (begin < N)
+		{
+			int end = begin + 1;
+			while (end < N && nums[end] == nums[end-1]+1)
+				++end;
+			//now summary [begin,end-1]
+			if (begin == end-1)
+				res.push_back(std::to_string(nums[begin]));
+			else
+				res.push_back(std::to_string(nums[begin])+std::string("->")+std::to_string(nums[end-1]));
+			begin = end;
+		}
+
+		std::cout << "SummaryRangesInSortedArray Better for \"" << Debug::ToStr1D<int>()(nums) << "\": " << Debug::ToStr1D<std::string>()(res) << std::endl;
+		return res;
+	}
 };
 /*
-SummaryRangesInSortedArray for "1, 3, 3, 5, 6, 8, 8, 9, 10, 10": 1, 3, 3, 5->6, 8, 8->10, 10
+SummaryRangesInSortedArray for "2, 3, 3, 4, 5, 7, 7, 9, 10, 13": 2->3, 3->5, 7, 7, 9->10, 13
+SummaryRangesInSortedArray Better for "1, 2, 2, 2, 3, 4, 5, 6, 10, 13": 1->2, 2, 2->6, 10, 13
 */
 #endif
