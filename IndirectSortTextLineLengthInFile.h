c19aa5912623966e2f31957d33eaeee87656f87f
Dave Feng <dave@deepnology.com>
2018-06-20 17:01:16 -0400

    IndirectSortTextLinesInLogFile

--- IndirectSortTextLineLengthInFile.h
+++ IndirectSortTextLineLengthInFile.h
@@ -79,4 +79,67 @@ abcde
 ijklmn
 mnopqrs
 */
+
+/*
+Amazon OA
+Sort log file lines.
+Each line contains an id (a mix of lowercase chars and digits) and followed by either all words or all numbers separated by single spaces.
+Lines with numbers should be rearrange to the end of the log with original order kept.
+Lines with words should be rearrange to the begin of the log based on the lexicographical order of the words.
+If there is a tie, order by the lexicographical order of their ids.
+ */
+class IndirectSortTextLinesInLogFile
+{
+public:
+	IndirectSortTextLinesInLogFile(){}
+
+	std::vector<std::string> Sort(const std::vector<std::string> & log)
+	{
+		std::vector<std::string> res;
+		int N = log.size();
+		std::vector<const std::string *> v;
+		for (auto & s : log)
+			v.push_back(&s);
+		std::sort(v.begin(), v.end(), [](const std::string * a, const std::string * b)->bool
+		{
+			int i = 0; int A = a->size();
+			while (i < A && (*a)[i] != ' ')
+				++i;
+			std::string id1 = a->substr(0, i);
+			std::string txt1 = a->substr(i+1);
+			while (i < A && (*a)[i] == ' ')
+				++i;
+			int type1 = i == A ? 0 : isdigit((*a)[i]) ? 1 : 2;
+			int j = 0; int B = b->size();
+			while (j < B && (*b)[j] != ' ')
+				++j;
+			std::string id2 = b->substr(0, j);
+			std::string txt2 = b->substr(j+1);
+			while (j < B && (*b)[j] == ' ')
+				++j;
+			int type2 = j == B ? 0 : isdigit((*b)[j]) ? 1 : 2;
+
+            //std::cout << id1 << "," << txt1 << ";" << id2 << "," << txt2 << std::endl;
+			if (type1 <= 1 && type2 <= 1)
+                return false;//!!!!!!!!!!!!!!!!!
+            else if (type1 <= 1)
+                return false;
+            else if (type2 <= 1)
+                return true;
+            else if (txt1 == txt2)
+                return id1 < id2;
+            else
+                return txt1 < txt2;
+		});
+        for (auto & ptr : v)
+            res.push_back(*ptr);
+
+        std::cout << "IndirectSortTextLinesInLogFile for [" << Debug::ToStr1D<std::string>()(log) << "]: " << std::endl << Debug::ToStr1D<std::string>()(res) << std::endl;
+		return res;
+	}
+};
+/*
+IndirectSortTextLinesInLogFile for [zz 93 1, a2da abd fjdks lkdf, b3ds gfi skl, sji akdk jige, d1ka 123 345 943 32, dkj dks jdf, d0k 8342 372 83, dki dks jdf]:
+a2da abd fjdks lkdf, sji akdk jige, dki dks jdf, dkj dks jdf, b3ds gfi skl, zz 93 1, d1ka 123 345 943 32, d0k 8342 372 83
+ */
 #endif
