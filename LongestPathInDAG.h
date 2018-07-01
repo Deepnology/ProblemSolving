77f5f4333a83b949eb59721c67a6ce71c32a5f11
Dave Feng <dave@deepnology.com>
2018-06-30 21:35:18 -0400

    longest dist from inDegree=0 nodes to all nodes

--- LongestPathInDAG.h
+++ LongestPathInDAG.h
@@ -23,6 +23,7 @@ class LongestPathInDAG
 public:
 	LongestPathInDAG(){}
 	~LongestPathInDAG(){}
+
 	void DFS_AdjacencyMatrix(const std::vector<std::vector<int> > & DAGadjMatrix, int source)
 	{
 		Debug::Print2D<int>()(DAGadjMatrix, false);
@@ -61,6 +62,54 @@ public:
 		}
 		Debug::Print2D<int>()(paths, false);
 		std::cout << "LongestPathInDAG DFS for source=\""<< source <<  "\", topologicalSort=\"" << topologicalSort << "\", distFromSource: " << Debug::ToStr1D<int>()(dist) << std::endl;
+	}
+	void DFS_AdjacencyMatrix(const std::vector<std::vector<int> > & DAGadjMatrix)
+	{
+		Debug::Print2D<int>()(DAGadjMatrix, false);
+		int N = DAGadjMatrix.size();
+		std::vector<int> visitIndex(N, -1);
+		int visitCount = -1;
+		std::stack<int> stk;
+		for (int i = 0; i < N; ++i)
+		{
+			if (visitIndex[i] == -1)
+			{
+				this->DFS_AdjacencyMatrix_Recur(DAGadjMatrix, i, visitCount, visitIndex, stk);
+			}
+		}
+		std::string topologicalSort = Debug::ToStr1D<int>()(stk);
+		std::vector<int> dist(N, INT_MIN);//dist[i]: max dist from inDegree=0 nodes to node i
+		std::vector<int> inDegree(N, 0);
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+				if (DAGadjMatrix[i][j])
+					++inDegree[j];
+		}
+		for (int i = 0; i < N; ++i)
+			if (inDegree[i] == 0)
+				dist[i] = 0;
+		std::vector<std::vector<int> > paths(N, std::vector<int>());
+		while (!stk.empty())
+		{
+			int v = stk.top();
+			stk.pop();
+			if (dist[v] != INT_MIN)
+			{
+				for (int i = 0; i < N; ++i)
+				{
+					if (DAGadjMatrix[v][i] > 0)
+						if (dist[i] < dist[v] + DAGadjMatrix[v][i])
+						{
+							dist[i] = dist[v] + DAGadjMatrix[v][i];
+							paths[i] = paths[v];
+							paths[i].push_back(v);
+						}
+				}
+			}
+		}
+		Debug::Print2D<int>()(paths, false);
+		std::cout << "LongestPathInDAG DFS for topologicalSort=\"" << topologicalSort << "\", dist From InDegree=0 Nodes: " << Debug::ToStr1D<int>()(dist) << std::endl;
 	}
 private:
 	void DFS_AdjacencyMatrix_Recur(const std::vector<std::vector<int> > & DAGadjMatrix, int v, int & visitCount, std::vector<int> & visitIndex, std::stack<int> & stk)
@@ -149,5 +198,25 @@ Row#7	= 1, 4, 6, 3, 2, 5
 Row#8	= 1, 4, 6, 3,  ,  
 
 LongestPathInDAG DFS for source="1", topologicalSort="7, 5, 2, 8, 3, 0, 6, 4, 1", distFromSource: 2, 0, 14, 11, 3, 20, 7, 25, 14
+
+0->1->2->3->4->5
+4->5
+[rY][cX]
+Row#0	= 0, 1, 0, 0, 0, 0
+Row#1	= 0, 0, 1, 0, 0, 0
+Row#2	= 0, 0, 0, 1, 0, 0
+Row#3	= 0, 0, 0, 0, 0, 1
+Row#4	= 0, 0, 0, 0, 0, 1
+Row#5	= 0, 0, 0, 0, 0, 0
+
+[rY][cX]
+Row#0	=  ,  ,  ,
+Row#1	= 0,  ,  ,
+Row#2	= 0, 1,  ,
+Row#3	= 0, 1, 2,
+Row#4	=  ,  ,  ,
+Row#5	= 0, 1, 2, 3
+
+LongestPathInDAG DFS for topologicalSort="5, 3, 2, 1, 0, 4", dist From InDegree=0 Nodes: 0, 1, 2, 3, 0, 4
 */
 #endif
