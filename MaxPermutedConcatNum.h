b2a3c3b82de512e1ee1eecbe918b6925d6c18d6b
Dave Feng <dave@deepnology.com>
2018-07-25 21:42:26 -0400

    new

--- MaxPermutedConcatNum.h
+++ MaxPermutedConcatNum.h
@@ -6,8 +6,7 @@
 #include <iostream>
 #include "Debug.h"
 /*
-https://leetcode.com/problems/largest-number/
-http://leetcodesolution.blogspot.tw/2015/01/leetcode-largest-number.html
+Leetcode: Largest Number
 Given a list of non negative integers, arrange them such that they form the largest number.
 For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
 Note: The result may be very large, so you need to return a string instead of an integer.
@@ -27,23 +26,34 @@ public:
 	std::string BySort(std::vector<int> && v)
 	{
 		std::string before = Debug::ToStr1D<int>()(v);
-		int N = v.size();
+		int n = v.size();
 
-		std::sort(v.begin(), v.end(), [](const int & a, const int & b)->bool
+		std::sort(v.begin(), v.end(), [](int x, int y)->bool
 		{
-			std::string s1 = std::to_string(a) + std::to_string(b);
-			std::string s2 = std::to_string(b) + std::to_string(a);
-			return s1 < s2;
+			std::string a = std::to_string(x);
+			std::string b = std::to_string(y);
+			int N = a.size();
+			int M = b.size();
+			int i = 0;
+			int j = 0;
+			while (i < N && j < M)
+			{
+				if (a[i] != b[i])
+					return a[i] < b[i];
+				++i; ++j;
+			}
+			//now a is a prefix of b OR b is a prefix of a
+			return a+b < b+a;
 		});
 
 		/*
 		for case [0, 0], if max is 0, then return "0" rather "00"
 		*/
-		if (v[N - 1] == 0)
+		if (v[n - 1] == 0)
 			return "0";
 
 		std::string res;
-		for (int i = N - 1; i >= 0; --i)
+		for (int i = n - 1; i >= 0; --i)
 			res.append(std::to_string(v[i]));
 
 		std::cout << Debug::ToStr1D<int>()(v) << std::endl;
