b4326d802dac3376adbbc870fcf24fd40a8993b4
Dave Feng <dave@deepnology.com>
2018-07-28 22:30:17 -0400

    new

--- FindMinCol1In01Matrix.h
+++ FindMinCol1In01Matrix.h
@@ -3,15 +3,16 @@
 #define _FIND_MIN_COL_1_IN_01_MATRIX_H
 #include "Debug.h"
 /*
-Given a 2d matrix with only 0 and 1.
-Find a 1 that has min column index.
+Given a 2d matrix with only 0 and 1, where all 0s in each row come before 1s.
+Find the row that contains max number of 1s, and return the leftmost column index of 1s in that row.
+(Find a 1 that has min column index)
  */
 class FindMinCol1In01Matrix
 {
 public:
     FindMinCol1In01Matrix(){}
 
-    int BinarySearchFromUpperRightCorner(const std::vector<std::vector<int>> & v)
+    int LinearSearchFromUpperRightCorner(const std::vector<std::vector<int>> & v)//O(N+M) time
     {
         int N = v.size();
         int M = v[0].size();
@@ -25,7 +26,7 @@ public:
                 ++i;
         }
         Debug::Print2D<int>()(v, false);
-        std::cout << "FindMinCol1In01Matrix BinarySearchFromUpperRightCorner for above matrix: " << j+1 << std::endl;
+        std::cout << "FindMinCol1In01Matrix LinearSearchFromUpperRightCorner for above matrix: " << j+1 << std::endl;
         return j+1;
     }
 };
@@ -36,13 +37,13 @@ Row#1	= 0, 1, 1, 1, 1
 Row#2	= 0, 0, 1, 1, 1
 Row#3	= 0, 0, 0, 0, 0
 
-FindMinCol1In01Matrix BinarySearchFromUpperRightCorner for above matrix: 1
+FindMinCol1In01Matrix LinearSearchFromUpperRightCorner for above matrix: 1
 [rY][cX]
 Row#0	= 0, 0, 0, 0, 1
 Row#1	= 0, 1, 1, 1, 1
 Row#2	= 0, 0, 1, 1, 1
 Row#3	= 1, 1, 1, 1, 1
 
-FindMinCol1In01Matrix BinarySearchFromUpperRightCorner for above matrix: 0
+FindMinCol1In01Matrix LinearSearchFromUpperRightCorner for above matrix: 0
  */
 #endif
