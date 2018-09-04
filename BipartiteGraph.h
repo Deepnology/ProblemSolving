d0acac6400e775bd4a2c8183e4432f3367d9e34d
Dave Feng <dave@deepnology.com>
2018-09-04 19:06:22 -0400

    possible bipartition

--- BipartiteGraph.h
+++ BipartiteGraph.h
@@ -132,4 +132,83 @@ public:
 		return true;
 	}
 };
+
+/*
+Leetcode: Possible Bipartition
+Given a set of N people (numbered 1, 2, ..., N), we would like to split everyone into two groups of any size.
+Each person may dislike some other people, and they should not go into the same group.
+Formally, if dislikes[i] = [a, b], it means it is not allowed to put the people numbered a and b into the same group.
+Return true if and only if it is possible to split everyone into two groups in this way.
+Example 1:
+Input: N = 4, dislikes = [[1,2],[1,3],[2,4]]
+Output: true
+Explanation: group1 [1,4], group2 [2,3]
+Example 2:
+Input: N = 3, dislikes = [[1,2],[1,3],[2,3]]
+Output: false
+Example 3:
+Input: N = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
+Output: false
+ */
+class PossibleBipartition
+{
+public:
+	PossibleBipartition(){}
+
+	bool Solve(int N, std::vector<std::vector<int>> & dislikes)
+	{
+		std::vector<std::unordered_set<int>> adjList(N+1, std::unordered_set<int>());
+		for (auto & p : dislikes)
+		{
+			adjList[p[0]].insert(p[1]);
+			adjList[p[1]].insert(p[0]);
+		}
+		std::vector<int> color(N+1, 0);
+		for (int i = 1; i <= N; ++i)
+		{
+			if (color[i] != 0) continue;
+			if (!DFS(adjList, i, color, 1))
+				return false;
+		}
+		return true;
+	}
+private:
+	bool BFS(const std::vector<std::unordered_set<int>> & adjList, int cur, std::vector<int> & color, int c)
+	{
+		color[cur] = c;
+		std::queue<int> que;
+		que.push(cur);
+		while (!que.empty())
+		{
+			cur = que.front(); que.pop();
+			c = color[cur];
+			for (int nxt : adjList[cur])
+			{
+				if (color[nxt] == c)
+					return false;
+				if (color[nxt] == 0)
+				{
+					color[nxt] = -c;
+					que.push(nxt);
+				}
+			}
+		}
+		return true;
+	}
+	bool DFS(const std::vector<std::unordered_set<int>> & adjList, int cur, std::vector<int> & color, int c)
+	{
+		color[cur] = c;
+		for (int nxt : adjList[cur])
+		{
+			if (color[nxt] == c)
+				return false;
+			if (color[nxt] == 0)
+			{
+				if (!DFS(adjList, nxt, color, -c))
+					return false;
+			}
+		}
+		return true;
+	}
+};
 #endif
