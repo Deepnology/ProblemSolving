2149ec324494422bb414c0e8709cd590dac3cb94
Dave Feng <dave@deepnology.com>
2019-04-02 15:36:51 -0400

    Shortest Distance to a Character

--- MinDistBtw2WordsInFile.h
+++ MinDistBtw2WordsInFile.h
@@ -135,6 +135,13 @@ public:
 		return minDist;
 	}
 };
+/*
+Leetcode: Shortest Distance to a Character
+Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+Example 1:
+Input: S = "loveleetcode", C = 'e'
+Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+ */
 class MinDistToTgtChar
 {
 public:
