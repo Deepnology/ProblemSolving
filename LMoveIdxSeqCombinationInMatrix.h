d4f3f17b8e601fb6176ceee045abc12a830d1530
Dave Feng <dave@deepnology.com>
2019-04-04 12:46:56 -0400

    Knight Dialer

--- LMoveIdxSeqCombinationInMatrix.h
+++ LMoveIdxSeqCombinationInMatrix.h
@@ -10,6 +10,14 @@ Given a matrix like this.
 .9.
 You can move L shape (0->5 or 7), (1->6 or 8), (3->2 or 8) starting from 0.
 Given number of moves, count number of different combination of index sequences.
+
+Leetcode: Knight Dialer
+A chess knight can move as indicated in the chess diagram below:
+This time, we place our chess knight on any numbered key of a phone pad (indicated above), and the knight makes N-1 hops.
+Each hop must be from one key to another numbered key.
+Each time it lands on a key (including the initial placement of the knight), it presses the number of that key, pressing N digits total.
+How many distinct numbers can you dial in this manner?
+Since the answer may be large, output the answer modulo 10^9 + 7.
  */
 class LMoveIdxSeqCombinationInMatrix
 {
