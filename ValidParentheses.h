3c68c318ad1877bd80f9296a62c0e5b1a019a4c0
Dave Feng <dave@deepnology.com>
2019-04-04 12:55:53 -0400

    Minimum Add To Make Parentheses Valid

--- ValidParentheses.h
+++ ValidParentheses.h
@@ -36,6 +36,27 @@ Example 3:
 Input: "(*))"
 Output: True
 This can be done using DFS.
+
+Leetcode: Minimum Add to Make Parentheses Valid
+Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions )
+so that the resulting parentheses string is valid.
+Formally, a parentheses string is valid if and only if:
+It is the empty string, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
+Example 1:
+Input: "())"
+Output: 1
+Example 2:
+Input: "((("
+Output: 3
+Example 3:
+Input: "()"
+Output: 0
+Example 4:
+Input: "()))(("
+Output: 4
 */
 class ValidParentheses
 {
@@ -303,6 +324,30 @@ public:
 			}
 		}
 		return true;
+	}
+};
+class ValidParenthesesMinAdd
+{
+public:
+	ValidParenthesesMinAdd(){}
+	~ValidParenthesesMinAdd(){}
+	int OnePass(std::string S)
+	{
+		int balance = 0;
+		int res = 0;
+		for (auto c : S)
+		{
+			if (c == '(')
+				++balance;
+			else
+				--balance;
+			if (balance < 0)
+			{
+				balance = 0;
+				++res;
+			}
+		}
+		return res + balance;
 	}
 };
 /*
