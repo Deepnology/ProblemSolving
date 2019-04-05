6484d09049a02f5aebd30e13c54fab53e87117bb
Dave Feng <dave@deepnology.com>
2019-04-05 13:04:03 -0400

    Check Completeness of a binary tree

--- CompleteBinaryTree.h
+++ CompleteBinaryTree.h
@@ -30,6 +30,11 @@ Write a function that returns the size of the largest subtree that is complete.
 Leetcode: Count Complete Tree Nodes
 Given a complete binary tree, count the number of nodes.
 
+Leetcode: Check Completeness of a Binary Tree
+Given a binary tree, determine if it is a complete binary tree.
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible.
+It can have between 1 and 2h nodes inclusive at the last level h.
+
 See also BalancedBinaryTree.h
 */
 class CompleteBinaryTree
