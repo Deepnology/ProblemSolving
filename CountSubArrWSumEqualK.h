700d87a53d23123f64f53c5720674dfad0ade2d0
Dave Feng <dave@deepnology.com>
2019-04-07 18:24:06 -0400

    Count01SubArrWSumEqualK

--- CountSubArrWSumEqualK.h
+++ CountSubArrWSumEqualK.h
@@ -106,4 +106,42 @@ CountSubArrWSumEqualK FindAll_PrefixSumMap for "1, 1, 1", k="2": [0,1], [1,2]
 CountSubArrWSumEqualK UsePrefixSumMap for "0, 1, 0, -2, 2, 1, 0, 0, -2", k="1": 16
 CountSubArrWSumEqualK FindAll_PrefixSumMap for "0, 1, 0, -2, 2, 1, 0, 0, -2", k="1": [0,1], [1,1], [0,2], [1,2], [0,4], [1,4], [2,5], [3,5], [5,5], [2,6], [3,6], [5,6], [2,7], [3,7], [5,7], [4,8]
 */
+/*
+Leetcode: Binary Subarrays with Sum
+In an array A of 0s and 1s, how many non-empty subarrays have sum S?
+Example 1:
+Input: A = [1,0,1,0,1], S = 2
+Output: 4
+Explanation:
+The 4 subarrays are bolded below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+ */
+class Count01SubArrWSumEqualK
+{
+public:
+	Count01SubArrWSumEqualK(){}
+	~Count01SubArrWSumEqualK(){}
+
+	int UsePrefixSumMap(std::vector<int> && A, int S)
+	{
+		//since A contains only 0 and 1, use array instead of unordered_map
+		int N = A.size();
+		std::vector<int> sumToCount(N+1, 0);
+		sumToCount[0] = 1;
+		int prefixSum = 0;
+		int res = 0;
+		for (int i = 0; i < N; ++i)
+		{
+			prefixSum += A[i];
+			if (prefixSum-S >= 0)
+				res += sumToCount[prefixSum-S];
+			++sumToCount[prefixSum];
+		}
+		std::cout << "Count01SubArrWSumEqualK UsePrefixSumMap for [" << Debug::ToStr1D<int>()(A) << "], " << S << ": " << res << std::endl;
+		return res;
+	}
+};
 #endif
