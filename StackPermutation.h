29860900d302fe0f0639261614ab742cc94abcdb
Dave Feng <dave@deepnology.com>
2019-04-12 17:09:56 -0400

    new

--- StackPermutation.h
+++ StackPermutation.h
@@ -6,11 +6,42 @@ https://www.geeksforgeeks.org/stack-permutations-check-if-an-array-is-stack-perm
 Given two integer arrays with unique numbers in each array,
 where one array represents the input order to a stack, and the other represents the output order out of the stack.
 Determine if the output array order is a valid order.
+Leetcode: Validate Stack Sequences
+Given two sequences pushed and popped with distinct values, return true if and only if
+ this could have been the result of a sequence of push and pop operations on an initially empty stack.
+Example 1:
+Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+Output: true
+Explanation: We might do the following sequence:
+push(1), push(2), push(3), push(4), pop() -> 4,
+push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+Example 2:
+Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+Output: false
+Explanation: 1 cannot be popped before 2.
  */
 class StackPermutation
 {
 public:
     StackPermutation(){}
+
+    bool Validate1(const std::vector<int> & pushed, const std::vector<int> & popped)
+    {
+        std::stack<int> stk;
+        int i = 0;
+        for (const int & x : pushed)
+        {
+            stk.push(x);
+            while (!stk.empty() && stk.top() == popped[i])
+            {
+                stk.pop();
+                ++i;
+            }
+        }
+        bool res = i == popped.size();
+        std::cout << "StackPermutation Validate1 for [" << Debug::ToStr1D<int>()(pushed) << "], [" << Debug::ToStr1D<int>()(popped) << "]: " << res << std::endl;
+        return res;
+    }
 
     bool Validate(const std::vector<int> & input, const std::vector<int> & output)
     {
@@ -49,11 +80,7 @@ public:
     }
 };
 /*
-StackPermutation Validate for [1, 2, 3, 4, 5], [5, 4, 3, 2, 1]: 1
-StackPermutation Validate for [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]: 1
-StackPermutation Validate for [5, 7, 8, 4, 6], [6, 8, 4, 7, 5]: 0
-StackPermutation Validate for [5, 7, 8, 4, 6], [6, 4, 5, 7, 8]: 0
-StackPermutation Validate for [5, 7, 8, 4, 6], [6, 4, 7, 8, 5]: 0
-StackPermutation Validate for [5, 7, 8, 4, 6], [7, 8, 4, 6, 5]: 1
+StackPermutation Validate1 for [1, 2, 3, 4, 5], [4, 5, 3, 2, 1]: 1
+StackPermutation Validate for [1, 2, 3, 4, 5], [4, 5, 3, 2, 1]: 1
  */
 #endif
