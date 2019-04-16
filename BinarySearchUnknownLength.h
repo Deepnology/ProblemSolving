9b53b1a898af1755f0b171cb05d98850649c2387
Dave Feng <dave@deepnology.com>
2019-04-16 13:03:33 -0400

    new

--- BinarySearchUnknownLength.h
+++ BinarySearchUnknownLength.h
@@ -18,6 +18,21 @@ Then we do a conventional binary search for k in the range [2^(p-1), 2^p-2].
 O(logn) time
 
 See also KthSmallestElementUnknownLength.h
+
+Leetcode: Search in a Sorted Array of Unknown Size
+Given an integer array sorted in ascending order, write a function to search target in nums.
+If target exists, then return its index, otherwise return -1.
+However, the array size is unknown to you.
+You may only access the array using an ArrayReader interface, where ArrayReader.get(k) returns the element of the array at index k (0-indexed).
+You may assume all integers in the array are less than 10000, and if you access the array out of bounds, ArrayReader.get will return 2147483647.
+Example 1:
+Input: array = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+Input: array = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
 */
 class BinarySearchUnknownLength
 {
@@ -85,6 +100,32 @@ public:
 
 		std::cout << "BinarySearchUnknownLength Iterate for \"" << target << "\" in \"" << Debug::ToStr1D<int>()(v) << "\": " << idx << std::endl;
 		return idx;
+	}
+
+	struct ArrayReader
+	{
+		int get(int idx) const
+		{
+			return 0;
+		}
+	};
+	int BinarySearch(const ArrayReader & reader, int target)
+	{
+		int right = 1;
+		while (reader.get(right) < target)
+			right <<= 1;
+		int left = right >> 1;
+		while (left <= right)
+		{
+			int mid = left + (right - left) / 2;
+			if (reader.get(mid) > target)
+				right = mid - 1;
+			else if (reader.get(mid) < target)
+				left = mid + 1;
+			else
+				return mid;
+		}
+		return -1;
 	}
 };
 /*
