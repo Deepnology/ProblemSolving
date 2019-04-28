32760b56962bbbcb6f7f5bfd8cdc068a65fb18b1
Dave Feng <dave@deepnology.com>
2019-04-28 16:48:48 -0400

    new

--- SpiralMatrix.h
+++ SpiralMatrix.h
@@ -38,6 +38,19 @@ You should return the following matrix:
 [ 7, 6, 5 ]
 ]
 
+Leetcode: Spiral Matrix III
+On a 2 dimensional grid with R rows and C columns, we start at (r0, c0) facing east.
+Here, the north-west corner of the grid is at the first row and column, and the south-east corner of the grid is at the last row and column.
+Now, we walk in a clockwise spiral shape to visit every position in this grid.
+Whenever we would move outside the boundary of the grid, we continue our walk outside the grid (but may return to the grid boundary later.)
+Eventually, we reach all R * C spaces of the grid.
+Return a list of coordinates representing the positions of the grid in the order they were visited.
+Example 1:
+Input: R = 1, C = 4, r0 = 0, c0 = 0
+Output: [[0,0],[0,1],[0,2],[0,3]]
+Example 2:
+Input: R = 5, C = 6, r0 = 1, c0 = 4
+Output: [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
 */
 class SpiralMatrix
 {
@@ -167,6 +180,33 @@ public:
 		Debug::Print2D<int>()(res, false);
 		return res;
 	}
+
+public:
+	//Spiral Matrix III
+	std::vector<std::vector<int>> Clockwise_RDLU_Path(int R, int C, int r0, int c0)
+	{
+		std::vector<std::vector<int>> dir({{0,1},{1,0},{0,-1},{-1,0}});//right,down,left,up
+		std::vector<std::vector<int>> res;
+		res.push_back({r0,c0});
+		int d = 0;
+		int len = 0;
+		while (res.size() < R*C)
+		{
+			if (d == 0 || d == 2) ++len;//when moving right or left
+			int move = len;
+			while (move-- > 0)
+			{
+				r0 += dir[d][0];
+				c0 += dir[d][1];
+				if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C)
+					res.push_back({r0,c0});
+			}
+			d = (d+1)%4;
+		}
+
+		std::cout << "SpiralMatrix Clockwise_RDLU_Path in [" << R << "," << C << "] starting at [" << r0 << "," << c0 << "]: " << Debug::ToStr1D<int>()(res) << std::endl;
+		return res;
+	}
 };
 /*
 [rY][cX]
@@ -206,5 +246,7 @@ Row#1	= 16, 17, 18, 19, 6
 Row#2	= 15, 24, 25, 20, 7
 Row#3	= 14, 23, 22, 21, 8
 Row#4	= 13, 12, 11, 10, 9
+
+SpiralMatrix Clockwise_RDLU_Path in [1,4] starting at [3,3]: [0,0], [0,1], [0,2], [0,3]
 */
 #endif
