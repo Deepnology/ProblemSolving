b87ca6c58e9de1ead8317becc9a29b239fa961f0
Dave Feng <dave@deepnology.com>
2019-04-28 22:19:00 -0400

    new

--- LongestSubArrWSameChars_AtMostKReplace.h
+++ LongestSubArrWSameChars_AtMostKReplace.h
@@ -131,4 +131,57 @@ public:
 LongestConsecu1s_AtMost1Replace SlideWindow for "1, 0, 1, 1, 0": 4
 LongestConsecu1s_AtMost1Replace FromStream for "1, 0, 1, 1, 0": 4
 */
+/*
+Leetcode: Max Consecutive Ones III
+Given an array A of 0s and 1s, we may change up to K values from 0 to 1.
+Return the length of the longest (contiguous) subarray that contains only 1s.
+Example 1:
+Input: A = [1,1,1,0,0,0,1,1,1,1,0], K = 2
+Output: 6
+Explanation:
+[1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1.  The longest subarray is underlined.
+Example 2:
+Input: A = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], K = 3
+Output: 10
+Explanation:
+[0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1.  The longest subarray is underlined.
+ */
+class LongestConsecu1s_AtMostKReplace
+{
+public:
+	LongestConsecu1s_AtMostKReplace(){}
+
+	//find longest subarr with at most K 0s
+	int SlideWindow(std::vector<int> && A, int K)
+	{
+		int N = A.size();
+		int begin = 0;
+		int count0 = 0;
+		int res = 0;
+		for (int end = 0; end < N; ++end)
+		{
+			if (A[end] == 0) ++count0;
+			//shrink window
+			while (count0 > K)
+			{
+				if (A[begin] == 0)
+				{
+					--count0;
+					++begin;
+				}
+				else
+					++begin;
+			}
+			res = std::max(res, end-begin+1);
+		}
+
+		std::cout << "LongestConsecu1s_AtMostKReplace SlideWindow for [" << Debug::ToStr1D<int>()(A) << "], K=" << K << ": " << res << std::endl;
+		return res;
+	}
+};
+/*
+LongestConsecu1s_AtMostKReplace SlideWindow for [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], K=2: 6
+ */
 #endif
