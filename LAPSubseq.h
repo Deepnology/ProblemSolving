82b9ea5da926d9c99e9e0008d6f94735992d82a1
Dave Feng <dave@deepnology.com>
2019-05-24 11:50:21 -0400

    new

--- LAPSubseq.h
+++ LAPSubseq.h
@@ -20,7 +20,7 @@ public:
 	~LAPSubseq(){}
 
 	//special case for 3 entries
-	bool ExistArithmeitcThree(const std::vector<int> & v)//O(n^2) time
+	bool ExistArithmeitcThree_Sorted(const std::vector<int> & v)//O(n^2) time
 	{
 		int N = v.size();
 		bool res = false;
@@ -45,11 +45,11 @@ public:
 				break;
 		}
 
-		std::cout << "LAPSubseq ExistArithmeticThree for \"" << Debug::ToStr1D<int>()(v) << "\": " << res << std::endl;
+		std::cout << "LAPSubseq ExistArithmeticThree_Sorted for \"" << Debug::ToStr1D<int>()(v) << "\": " << res << std::endl;
 		return res;
 	}
 
-	int Len_DP2D(const std::vector<int> & v)
+	int Len_DP2D_Sorted(const std::vector<int> & v)
 	{
 		int N = v.size();
 		std::vector<std::vector<int> > dp(N, std::vector<int>(N));
@@ -115,12 +115,40 @@ public:
 			}
 		}
 		Debug::Print2D<int>()(dp, false);
-		std::cout << "LAPSubseq Len_DP2D for \"" << Debug::ToStr1D<int>()(v) << "\": " << llap << std::endl;
+		std::cout << "LAPSubseq Len_DP2D_Sorted for \"" << Debug::ToStr1D<int>()(v) << "\": " << llap << std::endl;
 		return llap;
+	}
+
+	int Len_DP2D(const std::vector<int> & v)
+	{
+		int N = v.size();
+		std::vector<std::unordered_map<int,int>> dp(N, std::unordered_map<int,int>());
+		int res = 2;
+		for (int i = 1; i < N; ++i)
+		{
+			for (int j = 0; j < i; ++j)
+			{
+				long diff = (long)v[i] - (long)v[j];
+				if (diff > INT_MAX || diff < INT_MIN)
+					continue;
+				if (dp[j].count(diff))
+				{
+					dp[i][diff] = dp[j][diff]+1;
+					res = std::max(res, dp[i][diff]);
+				}
+				else
+				{
+					dp[i][diff] = 2;
+				}
+			}
+		}
+
+		std::cout << "LAPSubseq Len_DP2D for \"" << Debug::ToStr1D<int>()(v) << "\": " << res << std::endl;
+		return res;
 	}
 };
 /*
-LAPSubseq ExistArithmeticThree for "1, 7, 10, 13, 14, 19": 1
+LAPSubseq ExistArithmeticThree_Sorted for "1, 7, 10, 13, 14, 19": 1
 [rY][cX]
 Row#0	= 0, 4, 3, 2, 2, 2
 Row#1	= 0, 0, 3, 3, 2, 2
@@ -129,8 +157,8 @@ Row#3	= 0, 0, 0, 0, 2, 2
 Row#4	= 0, 0, 0, 0, 0, 2
 Row#5	= 0, 0, 0, 0, 0, 2
 
-LAPSubseq Len_DP2D for "1, 7, 10, 13, 14, 19": 4
-LAPSubseq ExistArithmeticThree for "1, 7, 10, 15, 27, 29": 1
+LAPSubseq Len_DP2D_Sorted for "1, 7, 10, 13, 14, 19": 4
+LAPSubseq ExistArithmeticThree_Sorted for "1, 7, 10, 15, 27, 29": 1
 [rY][cX]
 Row#0	= 0, 2, 2, 3, 2, 2
 Row#1	= 0, 0, 2, 2, 2, 2
@@ -139,8 +167,8 @@ Row#3	= 0, 0, 0, 0, 2, 2
 Row#4	= 0, 0, 0, 0, 0, 2
 Row#5	= 0, 0, 0, 0, 0, 2
 
-LAPSubseq Len_DP2D for "1, 7, 10, 15, 27, 29": 3
-LAPSubseq ExistArithmeticThree for "2, 4, 6, 8, 10": 1
+LAPSubseq Len_DP2D_Sorted for "1, 7, 10, 15, 27, 29": 3
+LAPSubseq ExistArithmeticThree_Sorted for "2, 4, 6, 8, 10": 1
 [rY][cX]
 Row#0	= 0, 5, 3, 2, 2
 Row#1	= 0, 0, 4, 2, 2
@@ -148,6 +176,7 @@ Row#2	= 0, 0, 0, 3, 2
 Row#3	= 0, 0, 0, 0, 2
 Row#4	= 0, 0, 0, 0, 2
 
-LAPSubseq Len_DP2D for "2, 4, 6, 8, 10": 5
+LAPSubseq Len_DP2D_Sorted for "2, 4, 6, 8, 10": 5
+LAPSubseq Len_DP2D for "20, 1, 15, 3, 10, 5, 8": 4
 */
 #endif
