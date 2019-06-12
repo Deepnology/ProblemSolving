e9d1483bc0706ca0a6cb781e855c9bc880c4e9e5
Dave Feng <dave@deepnology.com>
2019-06-11 23:05:05 -0400

    new

--- LMoveIdxSeqCombinationInMatrix.h
+++ LMoveIdxSeqCombinationInMatrix.h
@@ -18,6 +18,15 @@ Each hop must be from one key to another numbered key.
 Each time it lands on a key (including the initial placement of the knight), it presses the number of that key, pressing N digits total.
 How many distinct numbers can you dial in this manner?
 Since the answer may be large, output the answer modulo 10^9 + 7.
+Example 1:
+Input: 1
+Output: 10
+Example 2:
+Input: 2
+Output: 20
+Example 3:
+Input: 3
+Output: 46
  */
 class LMoveIdxSeqCombinationInMatrix
 {
@@ -82,6 +91,84 @@ public:
         int count = 0;
         for (auto & nxt : dest[cur])
             count += recur(nxt, move-1, dest);
+        return count;
+    }
+};
+class KnightDialer
+{
+public:
+    KnightDialer() = default;
+
+    int DP(int N)
+    {
+        std::vector<std::vector<int>> dest(10, std::vector<int>());//dest[i]: a list of indices that can be reached from index i
+        dest[0] = {5, 7};
+        dest[1] = {6, 8};
+        dest[2] = {3, 7};
+        dest[3] = {2, 8, 9};
+        dest[4] = {};
+        dest[5] = {0, 6, 9};
+        dest[6] = {1, 5};
+        dest[7] = {0, 2};
+        dest[8] = {1, 3};
+        dest[9] = {3, 5};
+
+        //DP: O(N) time
+        std::vector<std::vector<int>> dp(10, std::vector<int>(N+1, 0));//dp[i][j]: num of ways at index i at move j
+        for (int i = 0; i < 10; ++i)
+            dp[i][1] = 1;//start from index i
+        for (int moves = 2; moves <= N; ++moves)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                for (int nxt : dest[i])
+                {
+                    dp[nxt][moves] += dp[i][moves-1];
+                    dp[nxt][moves] %= 1000000007;
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < 10; ++i)
+        {
+            res += dp[i][N];
+            res %= 1000000007;
+        }
+        return res;
+        //there is a O(logN) solution
+    }
+
+    int Recur(int N)
+    {
+        std::vector<std::vector<int>> dest(10, std::vector<int>());//dest[i]: a list of indices that can be reached from index i
+        dest[0] = {5, 7};
+        dest[1] = {6, 8};
+        dest[2] = {3, 7};
+        dest[3] = {2, 8, 9};
+        dest[4] = {};
+        dest[5] = {0, 6, 9};
+        dest[6] = {1, 5};
+        dest[7] = {0, 2};
+        dest[8] = {1, 3};
+        dest[9] = {3, 5};
+        int res = 0;
+        for (int i = 0; i < 10; ++i)
+        {
+            res += recur(i, N, dest);
+            res %= 1000000007;
+        }
+        return res;
+    }
+    int recur(int cur, int move, const std::vector<std::vector<int>> & dest)
+    {
+        if (move == 1) return 1;
+        int count = 0;
+        for (auto & nxt : dest[cur])
+        {
+            count += recur(nxt, move-1, dest);
+            count %= 1000000007;
+        }
         return count;
     }
 };
