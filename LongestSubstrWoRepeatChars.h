7fd168f7217559ac7cadad682469c9d602102705
deepnology <deepnology@icloud.com>
2019-10-02 15:56:12 -0400

    substrwdistinctcharssizek

--- LongestSubstrWoRepeatChars.h
+++ LongestSubstrWoRepeatChars.h
@@ -65,9 +65,73 @@ public:
 		std::cout << Debug::ToStr1D<std::string>()(res) << std::endl;
 		return maxLen;
 	}
+
+	int FindFirstOne_SlideWindow2(const std::string & s) //better
+	{
+		int N = s.size();
+		int maxLen = 0;
+		int maxBegin = 0;
+		int begin = 0;
+		std::vector<std::string> res;
+		std::unordered_set<char> cur;
+		for (int end = 0; end < N; ++end)
+		{
+			while (cur.count(s[end]))
+			{
+				cur.erase(s[begin++]);
+			}
+			cur.insert(s[end]);
+
+			//record
+			if (end - begin + 1 > maxLen)
+			{
+				maxLen = end - begin + 1;
+				maxBegin = begin;
+			}
+
+			if (end - begin + 1 > 0)
+				res.push_back(s.substr(begin, end - begin + 1));
+		}
+
+		std::cout << "LongestSubstrWoRepeatChars FindFirstOne_SlideWindow for \"" << s << "\": " << maxLen << "(" << s.substr(maxBegin, maxLen) << ")" << std::endl;
+		std::cout << Debug::ToStr1D<std::string>()(res) << std::endl;
+		return maxLen;
+	}
+};
+class SubstrWDistinctCharsSizeK //Amazon OA
+{
+public:
+	SubstrWDistinctCharsSizeK(){}
+
+	std::vector<std::string> FindAll_SlideWindow(const std::string & s, int k)
+	{
+		int N = s.size();
+		int begin = 0;
+		std::unordered_set<std::string> res;
+		std::unordered_set<char> cur;
+		for (int end = 0; end < N; ++end)
+		{
+			while (cur.count(s[end]))
+			{
+				cur.erase(s[begin++]);
+			}
+			cur.insert(s[end]);
+
+			if (cur.size() == k)
+			{
+				res.insert(s.substr(begin, k));
+				cur.erase(s[begin++]);
+			}
+		}
+		std::vector<std::string> resV(res.begin(), res.end());
+		std::cout << "SubstrWDistinctCharsSizeK FindAll_SlideWindow for " << k << " from [" << s << "]: " << Debug::ToStr1D<std::string>()(resV) << std::endl;
+		return resV;
+	}
 };
 /*
 LongestSubstrWoRepeatChars FindFirstOne_SlideWindow for "abcdefgdijkxyz": 10(efgdijkxyz)
 a, ab, abc, abcd, abcde, abcdef, abcdefg, efgd, efgdi, efgdij, efgdijk, efgdijkx, efgdijkxy, efgdijkxyz
+
+SubstrWDistinctCharsSizeK FindAll_SlideWindow for 4 from [awaglknagawunagwkwagl]: kwag, agwk, unag, nagw, wuna, lkna, awun, aglk, knag, gawu, glkn, wagl
 */
 #endif
