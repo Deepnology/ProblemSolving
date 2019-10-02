de42c622f5edc083956a5a765f1623004d1e9ce9
deepnology <deepnology@icloud.com>
2019-10-02 12:39:19 -0400

    last stone weight

--- MinCostConnectRopes.h
+++ MinCostConnectRopes.h
@@ -7,6 +7,8 @@
 #include <algorithm>
 #include "Debug.h"
 /*
+Amazon OA
+Leetcode: Minimum Cost To Connect Sticks
 http://www.geeksforgeeks.org/connect-n-ropes-minimum-cost/
 There are given n ropes of different lengths, we need to connect these ropes into one rope. 
 The cost to connect two ropes is equal to sum of their lengths. 
@@ -53,4 +55,38 @@ public:
 MinCostConnectRopes MinHeap for "4, 3, 2, 6": 29. left in heap: 15
 MinCostConnectRopes MinHeap for "4, 3, 2, 6, 5, 9, 7, 8, 1": 135. left in heap: 45
 */
+/*
+Leetcode: Last Stone Weight
+We have a collection of rocks, each rock has a positive integer weight.
+Each turn, we choose the two heaviest rocks and smash them together.
+Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+Example 1:
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation:
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+ */
+class LastStoneWeight
+{
+public:
+    LastStoneWeight(){}
+    int MaxHeap(std::vector<int> && stones)
+    {
+        std::priority_queue<int, std::vector<int>, std::less<int>> maxHeap(stones.begin(), stones.end());
+        while (maxHeap.size() > 1)
+        {
+            int x = maxHeap.top(); maxHeap.pop();
+            int y = maxHeap.top(); maxHeap.pop();
+            if (x != y)
+                maxHeap.push(std::abs(x-y));
+        }
+        return maxHeap.empty() ? 0 : maxHeap.top();
+    }
+};
 #endif
