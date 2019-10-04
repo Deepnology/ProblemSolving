7cb79edea91f854535290f1e46c5ee5e9b26afd6
deepnology <deepnology@icloud.com>
2019-10-04 15:47:49 -0400

    High Five

--- MaxTop3AvgScoreStudent.h
+++ MaxTop3AvgScoreStudent.h
@@ -57,7 +57,43 @@ public:
 		std::cout << "MaxTop3AvgScoreStudent for \"" << before << "\": " << res << std::endl;
 		return res;
 	}
-
-
+};
+/*
+Leetcode: High Five
+Given a list of scores of different students, return the average score of each student's top five scores in the order of each student's id.
+Each entry items[i] has items[i][0] the student's id, and items[i][1] the student's score.
+The average score is calculated using integer division.
+Example 1:
+Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+Output: [[1,87],[2,88]]
+Explanation:
+The average of the student with id = 1 is 87.
+The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+Note:
+1 <= items.length <= 1000
+items[i].length == 2
+The IDs of the students is between 1 to 1000
+The score of the students is between 1 to 100
+For each student, there are at least 5 scores
+ */
+class TopFiveScoresAvgForEachStudent
+{
+public:
+	std::vector<std::vector<int>> Find(std::vector<std::vector<int>> & items)
+	{
+		std::map<int,std::multiset<int>> idToScores;
+		for (auto & p : items)
+		{
+			idToScores[p[0]].insert(p[1]);
+			if (idToScores[p[0]].size() > 5)
+				idToScores[p[0]].erase(idToScores[p[0]].begin());
+		}
+		std::vector<std::vector<int>> res;
+		for (auto & p : idToScores)
+		{
+			res.push_back({p.first, std::accumulate(p.second.begin(), p.second.end(), 0)/5});
+		}
+		return res;
+	}
 };
 #endif
