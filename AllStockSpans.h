5ad28cbae87d3895562e76b9f6be80fa1e5c7783
deepnology <deepnology@icloud.com>
2019-10-12 19:05:39 -0400

    onlinestockspan

--- AllStockSpans.h
+++ AllStockSpans.h
@@ -66,4 +66,43 @@ public:
 /*
 AllStockSpans SortedStack for "9, 7, 4, 5, 4, 5, 8": 1, 1, 1, 2, 1, 4, 6
 */
+/*
+Leetcode: Online Stock Span
+Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+Example 1:
+Input: ["StockSpanner","next","next","next","next","next","next","next"], [[],[100],[80],[60],[70],[60],[75],[85]]
+Output: [null,1,1,1,2,1,4,6]
+Explanation:
+First, S = StockSpanner() is initialized.  Then:
+S.next(100) is called and returns 1,
+S.next(80) is called and returns 1,
+S.next(60) is called and returns 1,
+S.next(70) is called and returns 2,
+S.next(60) is called and returns 1,
+S.next(75) is called and returns 4,
+S.next(85) is called and returns 6.
+Note that (for example) S.next(75) returned 4, because the last 4 prices
+(including today's price of 75) were less than or equal to today's price.
+ */
+class OnlineStockSpan
+{
+	std::stack<std::pair<int,int>> stk;//<price,span> where price sorted in strickly decr order
+public:
+	OnlineStockSpan() {}
+
+	int next(int price)
+	{
+		int span = 1;//span of cur price
+		while (!stk.empty() && price >= stk.top().first)
+		{
+			span += stk.top().second;
+			stk.pop();
+		}
+		//now price < stk.top().first
+		stk.push({price,span});
+		return span;
+	}
+};
 #endif
