a82c4b77c4f5fbfc7f7dc254a7d33109484a1e1b
deepnology <deepnology@icloud.com>
2019-10-12 18:11:14 -0400

    validate repeat remove substr abc

--- RepeatRemoveAdjDuplicatesInString.h
+++ RepeatRemoveAdjDuplicatesInString.h
@@ -77,4 +77,52 @@ public:
 RepeatRemoveAdjDuplicatesInString UseStack for [abbaca]: ca
 RepeatRemoveAdjDuplicatesInString UseStack for [deeedbbcccbdaa], k=3: aa
  */
+/*
+Leetcode: Check If Word Is Valid After Substitutions
+We are given that the string "abc" is valid.
+From any valid string V, we may split V into two pieces X and Y such that X + Y (X concatenated with Y) is equal to V.
+(X or Y may be empty.)  Then, X + "abc" + Y is also valid.
+If for example S = "abc", then examples of valid strings are: "abc", "aabcbc", "abcabc", "abcabcababcc".
+Examples of invalid strings are: "abccba", "ab", "cababc", "bac".
+Return true if and only if the given string S is valid.
+Example 1:
+Input: "aabcbc"
+Output: true
+Explanation:
+We start with the valid string "abc".
+Then we can insert another "abc" between "a" and "bc", resulting in "a" + "abc" + "bc" which is "aabcbc".
+Example 2:
+Input: "abcabcababcc"
+Output: true
+Explanation:
+"abcabcabc" is valid after consecutive insertings of "abc".
+Then we can insert "abc" before the last letter, resulting in "abcabcab" + "abc" + "c" which is "abcabcababcc".
+Example 3:
+Input: "abccba"
+Output: false
+Example 4:
+Input: "cababc"
+Output: false
+ */
+class ValidateRepeatRemoveSubstrABC
+{
+public:
+    bool UseStack(std::string S)
+    {
+        std::string stk;
+        int N = S.size();
+        for (int i = 0; i < N; ++i)
+        {
+            stk.push_back(S[i]);
+            if (stk.size() >= 3 && stk.substr(stk.size()-3) == "abc")
+                stk = stk.substr(0, stk.size()-3);
+        }
+        bool res = stk.empty();
+        std::cout << "ValidateRepeatRemoveSubstrABC UseStack for " << S << ": " << res << std::endl;
+        return res;
+    }
+};
+/*
+ValidateRepeatRemoveSubstrABC UseStack for abcabcababcc: 1
+ */
 #endif
