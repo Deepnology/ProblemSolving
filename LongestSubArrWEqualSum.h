24753440aee7419b49864f5fa748073c5582aa19
deepnology <deepnology@icloud.com>
2019-10-12 18:58:28 -0400

    longest sub arr w more greater than 8 num count

--- LongestSubArrWEqualSum.h
+++ LongestSubArrWEqualSum.h
@@ -75,4 +75,44 @@ public:
 LongestSubArrWEqualSum HashMap for "3" from "1, -1, 5, -2, 3": 4
 LongestSubArrWEqualSum HashMap for "1" from "-2, -1, 2, 1": 2
 */
+/*
+Leetcode: Longest Well-Performing Interval
+We are given hours, a list of the number of hours worked per day for a given employee.
+A day is considered to be a tiring day if and only if the number of hours worked is (strictly) greater than 8.
+A well-performing interval is an interval of days for which the number of tiring days is strictly larger than the number of non-tiring days.
+Return the length of the longest well-performing interval.
+Example 1:
+Input: hours = [9,9,6,0,6,6,9]
+Output: 3
+Explanation: The longest well-performing interval is [9,9,6].
+ */
+class LongestSubArrWMoreGreaterThan8NumCount
+{
+public:
+	int LongestOneAndMinusOneSubArrWPositiveSum(std::vector<int> && hours)
+	{
+		std::unordered_map<int,int> prefixSumMap;//<prefixSum, first occur index>
+		int N = hours.size();
+		int prefixSum = 0;
+		int maxLen = 0;
+		for (int i = 0; i < N; ++i)
+		{
+			prefixSum += hours[i] > 8 ? 1 : -1;
+			if (prefixSum > 0)
+				maxLen = std::max(maxLen, i+1);//from 0 to i
+			else if (prefixSumMap.count(prefixSum-1))
+				maxLen = std::max(maxLen, i-prefixSumMap[prefixSum-1]);
+			//ex: if prefixSum=0, find the first index w/ prefixSum=-1, such that subarr sum in [index+1:i] = 0-(-1) = 1, where the subarr len is i-index
+			//ex: if prefixSum=-1, find the first index w/ prefixSum=-2, such that subarr sum in [index+1:i] = -1-(-2) = 1, where the subarr len is i-index
+			if (!prefixSumMap.count(prefixSum))//record i if first seen prefixSum
+				prefixSumMap[prefixSum] = i;
+		}
+
+		std::cout << "LongestSubArrWMoreGreaterThan8NumCount LongestOneAndMinusOneSubArrWPositiveSum for [" << Debug::ToStr1D<int>()(hours) << "]: " << maxLen << std::endl;
+		return maxLen;
+	}
+};
+/*
+LongestSubArrWMoreGreaterThan8NumCount LongestOneAndMinusOneSubArrWPositiveSum for [9, 9, 6, 0, 6, 6, 9]: 3
+ */
 #endif
