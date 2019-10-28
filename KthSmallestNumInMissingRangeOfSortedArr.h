830411d1ed256ca9371ad6cc9cd8e95a08f3726f
deepnology <deepnology@icloud.com>
2019-10-28 16:34:24 -0400

    missing element in sorted array

--- KthSmallestNumInMissingRangeOfSortedArr.h
+++ KthSmallestNumInMissingRangeOfSortedArr.h
@@ -2,7 +2,23 @@
 #define _KTH_SMALLEST_NUM_IN_MISSING_RANGE_OF_SORTED_ARR_H
 #include "Debug.h"
 /*
-https://www.1point3acres.com/bbs/thread-525374-1-1.html
+Leetcode: Missing Element in Sorted Array
+Given a sorted array A of unique numbers, find the K-th missing number starting from the leftmost number of the array.
+Example 1:
+Input: A = [4,7,9,10], K = 1
+Output: 5
+Explanation:
+The first missing number is 5.
+Example 2:
+Input: A = [4,7,9,10], K = 3
+Output: 8
+Explanation:
+The missing numbers are [5,6,8,...], hence the third missing number is 8.
+Example 3:
+Input: A = [1,2,4], K = 3
+Output: 6
+Explanation:
+The missing numbers are [3,5,6,7,...], hence the third missing number is 6.
  */
 class KthSmallestNumInMissingRangeOfSortedArr
 {
@@ -28,7 +44,8 @@ public:
             }
         }
         //now i+1==j
-        int res = nums[i] + k >= nums[j] ? -1 : nums[i] + k;
+        //int res = nums[i] + k >= nums[j] ? -1 : nums[i] + k;
+        int res = nums[i] + k >= nums[j] ? nums[i]+k+1 : nums[i] + k;
 
         std::cout << "KthSmallestNumInMissingRangeOfSortedArr for " << kBefore << "th in [" << Debug::ToStr1D<int>()(nums) << "]: " << res << std::endl;
         return res;
