8e085168c879d82dd784d2ae892cbe6a2aa03c9f
deepnology <deepnology@icloud.com>
2020-10-09 13:45:28 -0400

    leetcode

--- PermuteCombinePartition/FullCombinationWOrder_ConsecutivePartition/SubArrSumPartition/PartitionArrMinimizeSumOfEachSubArrMax.h
+++ PermuteCombinePartition/FullCombinationWOrder_ConsecutivePartition/SubArrSumPartition/PartitionArrMinimizeSumOfEachSubArrMax.h
@@ -8,36 +8,74 @@ ex:
  [30, 10, 40, 20, 50], k=2: 80 ([30],[10,40,20,50] where sum=30+50=80)
  [74303, 20452, 66120, 44483, 5370, 68585], k=5: 234830 ([74303],[20452],[66120,44483],[5370],[68585] where sum=74303+20452+66120+5370+68585)
 
+Leetcode: Minimum Difficulty of a Job Schedule
+You want to schedule a list of jobs in d days. Jobs are dependent (i.e To work on the i-th job, you have to finish all the jobs j where 0 <= j < i).
+You have to finish at least one task every day. The difficulty of a job schedule is the sum of difficulties of each day of the d days. The difficulty of a day is the maximum difficulty of a job done in that day.
+Given an array of integers jobDifficulty and an integer d. The difficulty of the i-th job is jobDifficulty[i].
+Return the minimum difficulty of a job schedule. If you cannot find a schedule for the jobs return -1.
+
 see also PaintersPartitionFairWorkload.h
  */
 class PartitionArrMinimizeSumOfEachSubArrMax
 {
 public:
-
-    int Recur(std::vector<int> && v, int k)
+    //partition into d subarrays to minimize sum of max of each subarray
+    //DFS: O(N^3*d) time, O(N*d) space
+    int Recur1(std::vector<int> && jobDifficulty, int d)
     {
-        int res = recur(v, v.size(), k);
-        std::cout << "PartitionArrMinimizeSumOfEachSubArrMax Recur for [" << Debug::ToStr1D<int>()(v) << "], k=" << k << ": " << res << std::endl;
+        int N = jobDifficulty.size();
+        if (N < d) return -1;
+        std::vector<std::vector<int>> memo(N+1, std::vector<int>(d+1,-1));
+        int res = recur1(jobDifficulty, N, d, memo);
+        std::cout << "PartitionArrMinimizeSumOfEachSubArrMax Recur1 for [" << Debug::ToStr1D<int>()(jobDifficulty) << "], k=" << d << ": " << res << std::endl;
         return res;
     }
-private:
-    int recur(std::vector<int> & v, int curSize, int k)
+    int recur1(std::vector<int> & v, int curSize, int k, std::vector<std::vector<int>> & memo)
     {
+        if (memo[curSize][k] != -1)
+            return memo[curSize][k];
         if (k == 1)
-            return Max(v, 0, curSize-1);
-        if (curSize == 1)
-            return v[0];
+            return memo[curSize][k] = Max(v, 0, curSize-1);
+        //if (curSize == 1)
+        //    return memo[curSize][k] = v[0];
 
         int minMaxSum = INT_MAX;
         for (int leftSize = 1; leftSize < curSize; ++leftSize)
         {
             if (k-1 > leftSize) continue; //condition to make sure left array size >= k-1  !!!
-            minMaxSum = std::min(minMaxSum, recur(v, leftSize, k-1) + Max(v, leftSize, curSize-1));
+            minMaxSum = std::min(minMaxSum, recur1(v, leftSize, k-1, memo) +
+                                            Max(v, leftSize, curSize-1));
         }
-
-        return minMaxSum;
+        return memo[curSize][k] = minMaxSum;
     }
 
+    //improved DFS: O(N^2*d) time, O(N*d) space
+    int Recur2(std::vector<int> && jobDifficulty, int d)
+    {
+        int N = jobDifficulty.size();
+        if (N < d) return -1;
+        std::vector<std::vector<int>> memo(N+1, std::vector<int>(d+1,-1));
+        int res = recur2(jobDifficulty, N, d, memo);
+        std::cout << "PartitionArrMinimizeSumOfEachSubArrMax Recur2 for [" << Debug::ToStr1D<int>()(jobDifficulty) << "], k=" << d << ": " << res << std::endl;
+        return res;
+    }
+    int recur2(std::vector<int> & v, int curSize, int k, std::vector<std::vector<int>> & memo)
+    {
+        if (memo[curSize][k] != -1)
+            return memo[curSize][k];
+        if (k == 1)
+            return memo[curSize][k] = Max(v, 0, curSize-1);
+
+        int minMaxSum = INT_MAX;
+        int rightMax = INT_MIN;
+        for (int rightSize = 1; rightSize <= curSize-k+1; ++rightSize)
+        {
+            //note: leftSize must be >= k-1, so rightSize must be <= curSize-(k-1)
+            rightMax = std::max(rightMax, v[curSize-rightSize]);
+            minMaxSum = std::min(minMaxSum, recur2(v, curSize-rightSize, k-1, memo) + rightMax);
+        }
+        return memo[curSize][k] = minMaxSum;
+    }
     int Max(std::vector<int> & v, int begin, int end)
     {
         int res = v[begin];
@@ -45,6 +83,8 @@ private:
             res = std::max(res, v[i]);
         return res;
     }
+
+    //there are iterative DP solutions with O(N*d) time ....
 };
 /*
 PartitionArrMinimizeSumOfEachSubArrMax Recur for [30, 10, 40, 20, 50], k=2: 80
