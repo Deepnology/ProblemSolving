1e65c3acbea730c5835857745651372b4d59b763
deepnology <deepnology@icloud.com>
2020-10-09 18:50:41 -0400

    MaxNumOfNonOvlpSubstrWAllOccur_MinTotalLen.h

--- PermuteCombinePartition/FullCombinationWOrder_ConsecutivePartition/PartitionSameCharsInSingleSubstr.h
+++ PermuteCombinePartition/FullCombinationWOrder_ConsecutivePartition/PartitionSameCharsInSingleSubstr.h
@@ -14,6 +14,8 @@ Explanation:
 The partition is "ababcbaca", "defegde", "hijhklij".
 This is a partition so that each letter appears in at most one part.
 A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+
+see also MaxNumOfNonOvlpSubstrWAllOccur_MinTotalLen.h
  */
 class PartitionSameCharsInSingleSubstr
 {
