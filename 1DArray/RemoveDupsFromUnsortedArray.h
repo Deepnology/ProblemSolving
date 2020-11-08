cbb765986330dfd8c29c1ea946902d921a217114
deepnology <deepnology@icloud.com>
2020-11-08 14:00:35 -0500

    new

--- 1DArray/RemoveDupsFromUnsortedArray.h
+++ 1DArray/RemoveDupsFromUnsortedArray.h
@@ -13,7 +13,7 @@ Cracking the coding interview: Array and String
 Remove duplicated chars from a string in place.
 See also RemoveDuplicatesFromSortedArray.h
 
-Leetcode: Remove Duplicate Letters
+Leetcode: Remove Duplicate Letters, Smallest Subsequence of Distinct Characters
 Given a string which contains only lowercase letters, remove duplicate letters so that every letter appear once and only once.
 You must make sure your result is the smallest in lexicographical order among all possible results.
 Example:
