a6711ded4282d8c84e14300a1cc85cf2fc622121
deepnology <deepnology@icloud.com>
2021-02-14 12:34:30 -0500

    new LargestCombinationSumDiisibleBy3

---
+++ PermuteCombinePartition/PartialCombinationWoOrder/SingleBinAccumuSum/LargestCombinationSumDivisibleBy3.h
@@ -0,0 +1,39 @@
+#ifndef _LARGEST_COMBINATION_SUM_DIVISIBLE_BY_3_H
+#define _LARGEST_COMBINATION_SUM_DIVISIBLE_BY_3_H
+#include "Debug.h"
+/*
+Leetcode: Greatest Sum Divisible By Three
+Given an array nums of integers, we need to find the maximum possible sum of elements of the array such that it is divisible by three.
+Example 1:
+Input: nums = [3,6,5,1,8]
+Output: 18
+Explanation: Pick numbers 3, 6, 1 and 8 their sum is 18 (maximum sum divisible by 3).
+Example 2:
+Input: nums = [4]
+Output: 0
+Explanation: Since 4 is not divisible by 3, do not pick any number.
+Example 3:
+Input: nums = [1,2,3,4,4]
+Output: 12
+Explanation: Pick numbers 1, 3, 4 and 4 their sum is 12 (maximum sum divisible by 3).
+Constraints:
+1 <= nums.length <= 4 * 10^4
+1 <= nums[i] <= 10^4
+ */
+class LargestCombinationSumDivisibleBy3
+{
+public:
+    int DP(std::vector<int>& nums)
+    {
+        std::vector<int> dp({0,0,0});
+        int N = nums.size();
+        for (int i = 0; i < N; ++i)
+        {
+            std::vector<int> tmp = dp;
+            for (int sum : tmp)
+                dp[(nums[i]+sum)%3] = std::max(dp[(nums[i]+sum)%3], nums[i]+sum);
+        }
+        return dp[0];
+    }
+};
+#endif
